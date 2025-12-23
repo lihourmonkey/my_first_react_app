885e662274bff43e9c19775695b8c19ca34094b2
lihourmonkey <lihourmonkey@outlook.com>
2025-12-23 23:29:56 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='text-4xl bg-blue-500 p-5 text-center text-amber-50'>
+      I am Chhay Lihour
+    </div>
+  )
+}
+
+export default App
